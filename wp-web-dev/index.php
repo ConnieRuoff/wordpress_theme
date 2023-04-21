5b82b14857b84d65e9e99ff576cb3b188e99288b
ConnieRuoff <104868276+ConnieRuoff@users.noreply.github.com>
2023-04-21 23:17:02 +0200

    as

--- wp-web-dev/index.php
+++ wp-web-dev/index.php
@@ -5,9 +5,9 @@
         <section id=""content"> <!-- The main information panel for our theme -->
 
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <section <?php post_class(); ?> id="post-<?php the_ID(); ?>"> 
+                <section <?php post_class(); ?> id="post-<?php the_ID(); ?>"> <!-- opening line for headline and content -->
 
-                <h2 class="wp-title"><a href="<?php the_permalink(); ?>"> <?php the_title();?> </a> </h2>
+                <h2 class="wp-title"><a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h2>
                 
                     <?php the_content(); ?>
 
@@ -15,15 +15,27 @@
 
                 <?php endwhile; ?>
 
-                <section class="post-nav">
+                <section class="post-nav"><!--Opening line for post-navigation -->
 
                      <p><?php previous_post_link( "%link", "Previous Post" ); ?></p>
-                     <p><?php next_post_link( "%link", "Next Post" ) ?></p>
+                     <p><?php next_post_link( "%link", "Next Post" ); ?></p>
+
+                </section> 
+
+                <?php else : ?>
+
+                <section <?php post_class()?> id="post-<?php the_ID(); ?>"></section> <!-- opening line not found -->
+
+
+                    <h3>Sorry couldn't find what you are looking for!</h3>
+
+                </section><!--closing line not found>
 
                 <?php endif; ?>
+
+        </section>        
         
-           
-""
+
            
 
         <!-- Placeholder for sidebar -->
@@ -32,5 +44,5 @@
             <p> the sidebar is working </p>
 
          <?php get_footer(); ?>  
-        </section> 
+       
     </section> <!-- closes sectiion-->
